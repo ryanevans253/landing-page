b6aa662f5238ea5bfa5f6ad3c66f0ed9d333229c
Ryan <ryanevans253@yahoo.com>
2022-04-13 22:58:38 -0400

    upload

---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+/** @jsx jsx */
+import { jsx, Image } from "theme-ui";
+import { Link } from "components/link";
+
+export default function Logo({ src, ...rest }) {
+  return (
+    <Link
+      path="/"
+      sx={{ variant: "links.logo", display: "flex", curson: "pointer", mr: 15 }}
+      {...rest}
+    >
+      <Image src={src} alt="logo" />
+    </Link>
+  );
+}
